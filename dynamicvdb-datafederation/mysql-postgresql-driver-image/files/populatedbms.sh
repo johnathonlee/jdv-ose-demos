a64bd494e899e4b07e485be6b213829ef22378eb
Cojan van Ballegooijen <cvanball@redhat.com>
2016-12-11 19:27:23 +0100

    Initial commit

--- dynamicvdb-datafederation/mysql-postgresql-driver-image/files/populatedbms.sh
+++ dynamicvdb-datafederation/mysql-postgresql-driver-image/files/populatedbms.sh
@@ -3,7 +3,7 @@
 set -x
 
 echo "Post PostgreSQL populating 'uscustomers' database"
-PGPASSWORD=$POSTGRESQL_PASSWORD /usr/bin/psql -h $POSTGRESQL_SERVICE_HOST -d $POSTGRESQL_DATABASE -U $POSTGRESQL_USERNAME -f ${HOME}/source/extensions/extras/uscustomers-psql.sql
+PGPASSWORD=$POSTGRESQL_PASSWORD /usr/bin/psql -h $USCUSTOMERS_SERVICE_HOST -d $POSTGRESQL_DATABASE -U $POSTGRESQL_USERNAME -f ${HOME}/source/extensions/extras/uscustomers-psql.sql
 
 echo "Post MySQL populating 'eucustomers' database"
-/usr/bin/mysql -u $MYSQL_USERNAME --password="$MYSQL_PASSWORD" -D $MYSQL_DATABASE -h $MYSQL_SERVICE_HOST < ${HOME}/source/extensions/extras/eucustomers-mysql.sql
+/usr/bin/mysql -u $MYSQL_USERNAME --password="$MYSQL_PASSWORD" -D $MYSQL_DATABASE -h $EUCUSTOMERS_SERVICE_HOST < ${HOME}/source/extensions/extras/eucustomers-mysql.sql
